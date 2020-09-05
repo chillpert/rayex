0790379b5c47c9d2b25132b050c6dad0aea449c4
chillpert <dev@christianhilpert.com>
2020-09-05 16:48:19 +0200

    documented command buffer

--- docs/html/dir_948f00e782b8c85615d95d9a903e4ec8.js
+++ docs/html/dir_948f00e782b8c85615d95d9a903e4ec8.js
@@ -3,9 +3,7 @@ var dir_948f00e782b8c85615d95d9a903e4ec8 =
     [ "Buffer.cpp", "_buffer_8cpp.html", null ],
     [ "Buffer.hpp", "_buffer_8hpp.html", null ],
     [ "CommandBuffer.cpp", "_command_buffer_8cpp.html", null ],
-    [ "CommandBuffer.hpp", "_command_buffer_8hpp.html", [
-      [ "CommandBuffer", "classrx_1_1_command_buffer.html", "classrx_1_1_command_buffer" ]
-    ] ],
+    [ "CommandBuffer.hpp", "_command_buffer_8hpp.html", null ],
     [ "IndexBuffer.hpp", "_index_buffer_8hpp.html", null ],
     [ "UniformBuffer.cpp", "_uniform_buffer_8cpp.html", null ],
     [ "UniformBuffer.hpp", "_uniform_buffer_8hpp.html", null ],
--- src/api/buffers/CommandBuffer.hpp
+++ src/api/buffers/CommandBuffer.hpp
@@ -5,30 +5,63 @@
 
 namespace rx
 {
+  /// A wrapper class for Vulkan command buffers.
+  /// @ingroup API
   class CommandBuffer
   {
   public:
     CommandBuffer( ) = default;
+    
+    /// @param commandPool The command pool from which the command buffers will be allocated from.
+    /// @param count The amount of Vulkan command buffers to initialize.
+    /// @param usageFlags Specifies what the buffer will be used for.
+    /// @param initialize If true, the command buffers will be initialized right away without an additional call to init().
     CommandBuffer( vk::CommandPool commandPool, uint32_t count = 1, vk::CommandBufferUsageFlags usageFlags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit, bool initialize = true );
 
+    /// Creates the command buffers.
+    /// @param commandPool The command pool from which the command buffers will be allocated from.
+    /// @param count The amount of Vulkan command buffers to initialize (the same as the amount of images in the swapchain).
+    /// @param usageFlags Specifies what the buffer will be used for.
     void init( vk::CommandPool commandPool, uint32_t count = 1, vk::CommandBufferUsageFlags usageFlags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit );
 
+    /// @return Returns the vector of command buffers.
     inline const std::vector<vk::CommandBuffer> get( ) const { return m_commandBuffers; }
-    inline const vk::CommandBuffer get( size_t index ) const { return m_commandBuffers[index]; }
 
+    /// Returns the command buffer by some index.
+    /// @param index The index of the desired command buffer.
+    /// @return Returns the command buffer.
+    inline const vk::CommandBuffer get( size_t index ) const
+    {
+      RX_ASSERT( ( index <= m_commandBuffers.size( ) - 1 ), "Failed to get command buffer because vector is out of range." );
+      return m_commandBuffers[index];
+    }
+
+    /// Frees the command buffer.
     void free( );
+    
+    /// Resets the command buffer.
     RX_API void reset( );
 
+    /// Used to begin the command buffer recording.
+    /// @param index An index to a command buffer to record to.
     void begin( size_t index = 0 );
+    
+    /// Used to stop the command buffer recording.
+    /// @param index An index to a command buffer to stop recording.
     void end( size_t index = 0 );
 
+    /// Submits the recorded commands to a queue.
+    /// @param queue The queue to submit to.
+    /// @param waitSemaphores A vector of semaphores to wait for.
+    /// @param signalSemaphores A vector of semaphores to signal.
+    /// @waitDstStageMask The pipeline stage where the commands will be executed.
     void submitToQueue( vk::Queue queue, const std::vector<vk::Semaphore>& waitSemaphores = { }, const std::vector<vk::Semaphore>& signalSemaphores = { }, vk::PipelineStageFlags* waitDstStageMask = { } );
 
   private:
-    std::vector<vk::CommandBuffer> m_commandBuffers;
+    std::vector<vk::CommandBuffer> m_commandBuffers; ///< A vector of Vulkan command buffers.
     
-    vk::CommandPool m_commandPool;
-    vk::CommandBufferBeginInfo m_beginInfo;
+    vk::CommandPool m_commandPool; ///< The Vulkan command pool used to allocate the command buffer from.
+    vk::CommandBufferBeginInfo m_beginInfo; ///< The Vulkan begin information of the command buffer.
   };
 }
 

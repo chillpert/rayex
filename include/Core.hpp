5d15c6a50144455203bb22724230750e4953eed2
chillpert <dev@christianhilpert.com>
2020-10-21 12:10:20 +0200

    updated tinyLogger version

--- include/Core.hpp
+++ include/Core.hpp
@@ -38,24 +38,24 @@
 
 #define RAYEX_NAMESPACE_STRINGIFIED RAYEX_NAMESPACE_STRINGIFY( RAYEX_NAMESPACE )
 
-#define RX_VERBOSE( ... ) LOGGER_NAMESPACE::verbose( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
-#define RX_INFO( ... )    LOGGER_NAMESPACE::info( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
-#define RX_SUCCESS( ... ) LOGGER_NAMESPACE::success( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
-#define RX_WARN( ... )    LOGGER_NAMESPACE::warning( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
-#define RX_ERROR( ... )   LOGGER_NAMESPACE::error( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
-#define RX_FATAL( ... )   LOGGER_NAMESPACE::fatal( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+#define RX_VERBOSE( ... ) LOGGER_NAME::verbose( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+#define RX_INFO( ... )    LOGGER_NAME::info( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+#define RX_SUCCESS( ... ) LOGGER_NAME::success( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+#define RX_WARN( ... )    LOGGER_NAME::warning( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+#define RX_ERROR( ... )   LOGGER_NAME::error( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+#define RX_FATAL( ... )   LOGGER_NAME::fatal( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
 // Using redundant boolean literal in ternary expression to support vulkan object error check.
-#define RX_ASSERT( statement, ... ) LOGGER_NAMESPACE::assert2( statement ? true : false, RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+#define RX_ASSERT( statement, ... ) LOGGER_NAME::assert2( statement ? true : false, RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
 
 #define RX_LOG_TIME_START( ... )      \
   std::cout << std::endl;             \
   float startTime = Time::getTime( ); \
-  LOGGER_NAMESPACE::info( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
+  LOGGER_NAME::info( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__ )
 
-#define RX_LOG_TIME_STOP( ... ) LOGGER_NAMESPACE::success( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__, " ( Time: ", std::setprecision( std::numeric_limits<float>::digits10 ), Time::getTime( ) - startTime, " ms )." )
+#define RX_LOG_TIME_STOP( ... ) LOGGER_NAME::success( RAYEX_NAMESPACE_STRINGIFIED, ": ", __VA_ARGS__, " ( Time: ", std::setprecision( std::numeric_limits<float>::digits10 ), Time::getTime( ) - startTime, " ms )." )
 
 // Doxygen groups
-/// @defgroup Base Interfaces
+/// @defgroup BASE Base
 /// @defgroup API Vulkan
 
 #endif // CORE_HPP
